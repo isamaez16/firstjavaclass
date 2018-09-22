060de571fec3331656407123b9c480fc1fd4ad7a
isamaez16 <43502917+isamaez16@users.noreply.github.com>
2018-09-22 12:24:23 -0700

    FIrst Commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{ 
+  public static void main (String [] args) {
+    String s = "Hello World";
+    System.out.println(s);
+    
+    int number = 9;
+    int number2 = 10;
+    System.out.println(number + number2);
+//    double num = 9.0;
+//    double number4 = 9.1;
+    System.out.println ((double)number/number2);
+    
+    
+  }
+}
+                                           
